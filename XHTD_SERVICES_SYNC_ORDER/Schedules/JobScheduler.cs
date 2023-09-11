01594f894dc5974976c378057738b43a8e4a108e
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2023-09-11 20:54:30 +0700

    Update

--- XHTD_SERVICES_SYNC_ORDER/Schedules/JobScheduler.cs
+++ XHTD_SERVICES_SYNC_ORDER/Schedules/JobScheduler.cs
@@ -27,37 +27,15 @@ namespace XHTD_SERVICES_SYNC_ORDER.Schedules
             await _scheduler.Start();
 
             // Đồng bộ đơn hàng
-            IJobDetail syncInProgressOrderJob = JobBuilder.Create<SyncInProgressOrderJob>().Build();
-            ITrigger syncInProgressOrderTrigger = TriggerBuilder.Create()
+            IJobDetail syncOrderJob = JobBuilder.Create<SyncOrderJob>().Build();
+            ITrigger syncOrderTrigger = TriggerBuilder.Create()
                 .WithPriority(1)
                  .StartNow()
                  .WithSimpleSchedule(x => x
                      .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Sync_Order_Interval_In_Seconds")))
                     .RepeatForever())
                 .Build();
-            await _scheduler.ScheduleJob(syncInProgressOrderJob, syncInProgressOrderTrigger);
-
-            // Đồng bộ đơn hàng booked
-            IJobDetail syncBookedOrderJob = JobBuilder.Create<SyncBookedOrderJob>().Build();
-            ITrigger syncBookedOrderTrigger = TriggerBuilder.Create()
-                .WithPriority(1)
-                 .StartNow()
-                 .WithSimpleSchedule(x => x
-                     .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Sync_Booked_Order_Interval_In_Seconds")))
-                    .RepeatForever())
-                .Build();
-            await _scheduler.ScheduleJob(syncBookedOrderJob, syncBookedOrderTrigger);
-
-            // Đồng bộ đơn hàng changed
-            IJobDetail syncChangedOrderJob = JobBuilder.Create<SyncChangedOrderJob>().Build();
-            ITrigger syncChangedOrderTrigger = TriggerBuilder.Create()
-                .WithPriority(1)
-                 .StartNow()
-                 .WithSimpleSchedule(x => x
-                     .WithIntervalInSeconds(Convert.ToInt32(ConfigurationManager.AppSettings.Get("Sync_Booked_Changed_Interval_In_Seconds")))
-                    .RepeatForever())
-                .Build();
-            await _scheduler.ScheduleJob(syncChangedOrderJob, syncChangedOrderTrigger);
+            await _scheduler.ScheduleJob(syncOrderJob, syncOrderTrigger);
         }
     }
 }
