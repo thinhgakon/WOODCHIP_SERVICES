99796f43eea3a7170d5af5c8a09669491023dfd6
thinhgakon <toi.la.dan.xu.nghe@gmail.com>
2023-09-11 20:44:58 +0700

    Update code

--- XHTD_DEBUG/Program.cs
+++ XHTD_DEBUG/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Autofac;
 using log4net;
-using XHTD_SERVICES_PING;
-using XHTD_SERVICES_PING.Schedules;
+using XHTD_SERVICES_SYNC_ORDER;
+using XHTD_SERVICES_SYNC_ORDER.Schedules;
 
 namespace XHTD_DEBUG
 {
