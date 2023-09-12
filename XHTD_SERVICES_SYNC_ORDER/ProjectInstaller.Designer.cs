a2fdd38d536fa9a1a518466706c7338aefe5b8c4
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2023-09-12 10:50:03 +0700

    Update service name

--- XHTD_SERVICES_SYNC_ORDER/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_SYNC_ORDER/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "HPSyncOrderService";
+            this.serviceInstaller1.ServiceName = "VJCSyncOrderService";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
