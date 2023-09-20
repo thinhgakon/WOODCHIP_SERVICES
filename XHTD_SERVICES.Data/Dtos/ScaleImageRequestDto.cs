e4777d99a3076ce81a79ac6c467253d528438898
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2023-09-20 14:27:51 +0700

    Add request

---
+++ XHTD_SERVICES.Data/Dtos/ScaleImageRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XHTD_SERVICES.Data.Dtos
+{
+    public class ScaleImageRequestDto
+    {
+        public string ModuleType { get; set; }
+
+        public string Code { get; set; }
+
+        public string ScaleCode { get; set; }
+
+        public byte[] File { get; set; }
+    }
+}
