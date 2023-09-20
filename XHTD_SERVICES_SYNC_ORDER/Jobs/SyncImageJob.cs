baa3831aca1c8b9843e4f553e6dada4272068817
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2023-09-20 08:25:08 +0700

    Add SyncImage job

---
+++ XHTD_SERVICES_SYNC_ORDER/Jobs/SyncImageJob.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using log4net;
+using XHTD_SERVICES.Data.Repositories;
+using RestSharp;
+using XHTD_SERVICES_SYNC_ORDER.Models.Response;
+using XHTD_SERVICES.Data.Models.Response;
+using Newtonsoft.Json;
+using XHTD_SERVICES_SYNC_ORDER.Models.Values;
+using XHTD_SERVICES.Helper;
+using XHTD_SERVICES.Helper.Models.Request;
+using System.Threading;
+using XHTD_SERVICES.Data.Entities;
+
+namespace XHTD_SERVICES_SYNC_ORDER.Jobs
+{
+    public class SyncImageJob : IJob
+    {
+        protected readonly ScaleBillRepository _scaleBillRepository;
+
+        protected readonly SyncOrderLogger _syncOrderLogger;
+
+        private static string strToken;
+
+        protected const string SERVICE_ACTIVE_CODE = "SYNC_ORDER_ACTIVE";
+
+        protected const string SYNC_ORDER_HOURS = "SYNC_ORDER_HOURS";
+
+        public SyncImageJob(
+            ScaleBillRepository scaleBillRepository,
+            SyncOrderLogger syncOrderLogger
+            )
+        {
+            _scaleBillRepository = scaleBillRepository;
+            _syncOrderLogger = syncOrderLogger;
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            await Task.Run(async () =>
+            {
+                await SyncOrderProcess();
+            });
+        }
+
+        public async Task SyncOrderProcess()
+        {
+            _syncOrderLogger.LogInfo("Start process Sync Order service");
+
+            GetToken();
+
+            List<ScaleBillDto> scaleBills = _scaleBillRepository.GetList();
+
+            if (scaleBills == null || scaleBills.Count == 0)
+            {
+                _syncOrderLogger.LogInfo("Tất cả phiếu đã được đồng bộ");
+                return;
+            }
+
+            bool isSynced = await SyncScaleBillToDMS(scaleBills);
+        }
+
+        public void GetToken()
+        {
+            try
+            {
+                IRestResponse response = HttpRequest.GetWebsaleToken();
+
+                var content = response.Content;
+
+                var responseData = JsonConvert.DeserializeObject<GetMmesTokenResponse>(content);
+                strToken = responseData?.data?.accessToken;
+            }
+            catch (Exception ex)
+            {
+                _syncOrderLogger.LogInfo("getToken error: " + ex.Message);
+            }
+        }
+
+        public async Task<bool> SyncScaleBillToDMS(List<ScaleBillDto> scaleBills)
+        {
+            IRestResponse response = HttpRequest.SyncScaleBillToDMS(strToken, scaleBills);
+
+            var content = response.Content;
+
+            var responseData = JsonConvert.DeserializeObject<GetSyncResponse>(content);
+
+            var successList = responseData.data?.success;
+
+            var failList = responseData.data?.fails;
+
+            if(successList != null) { 
+                foreach ( var itemSuccess in successList )
+                {
+                    _syncOrderLogger.LogInfo($"Đồng bộ thành công: {itemSuccess.Code}");
+                    await this._scaleBillRepository.UpdateSyncSuccess(itemSuccess.Code);
+                }
+            }
+
+            if(failList != null) { 
+                foreach (var itemFail in failList)
+                {
+                    _syncOrderLogger.LogInfo($"Đồng bộ thất bại: {itemFail.Code}");
+                    await this._scaleBillRepository.UpdateSyncFail(itemFail.Code);
+                }
+            }
+
+            return true;
+        }
+    }
+}
