be64efe230e15bcc7b4a325569d07ad82b0e795c
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2024-01-02 14:07:40 +0700

    Đổi tên service

--- XHTD_SERVICES_GATEWAY/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_GATEWAY/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "HPGatewayService";
+            this.serviceInstaller1.ServiceName = "VJCGatewayService";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
