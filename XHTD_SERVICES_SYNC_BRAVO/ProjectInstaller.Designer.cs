ee6d56cf941a0d55887a9bdbd3b32ee387e50f59
DESKTOP-1H9TILT\THINH-BU <toi.la.dan.xu.nghe@gmail.com>
2024-01-03 08:34:16 +0700

    Đổi tên service: sync bravo

--- XHTD_SERVICES_SYNC_BRAVO/ProjectInstaller.Designer.cs
+++ XHTD_SERVICES_SYNC_BRAVO/ProjectInstaller.Designer.cs
@@ -39,7 +39,7 @@
             // 
             // serviceInstaller1
             // 
-            this.serviceInstaller1.ServiceName = "VJCSyncOrderService";
+            this.serviceInstaller1.ServiceName = "VJCSyncBravoService";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
             // ProjectInstaller
